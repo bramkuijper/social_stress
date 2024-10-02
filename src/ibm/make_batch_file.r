5d0d550578062c6d1eda55048d3782ecebd7ea29
Emily Gilford <151047174+emilygilford@users.noreply.github.com>
2024-10-02 11:40:28 +0000

    Committing addition of date time to batch file

--- src/ibm/make_batch_file.r
+++ src/ibm/make_batch_file.r
@@ -1,5 +1,7 @@
 #!/usr/bin/env Rscript
 
+library(datetime)
+
 exe <- "./stress_social.exe"
 file_name_prefix <- "sim_stress_social"
 
@@ -11,19 +13,25 @@ n = 20
 
 ctr <- 0
 
+# date time addition
 
-
-# TODO
-# make this date time dependent using something like datetime
+current_time <- now()
+formatted_time <- format(current_time, "%Y%m%d_%H%M%S")
 
 for (s_np_i in s_np) 
 {
     for (s_p_i in s_p) 
     {
         ctr <- ctr + 1
+
+        # output file name
+        output_file_name <- paste0(file_name_prefix,"_",ctr,"_",formatted_time,".txt") 
+        # assuming it needs a file type? There wasn't one before
+
         writeLines(text = paste0(exe," ",file_name_prefix
                         ,"_",ctr," ",s_np_i
-                        , " ", s_p_i, " ",npatches, " ",n))
+                        , " ", s_p_i, " ",npatches, " ",n),
+                        con = output_file_name)
     }
 }
 
