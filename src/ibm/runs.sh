e5cfd3eafb91ab0486864a52f6c1383d900f92a2
emilygilford <emilygilford@users.noreply.github.com>
2024-10-22 15:38:03 +0100

    committing, but existing issues with ntotal assert warning

--- src/ibm/runs.sh
+++ src/ibm/runs.sh
@@ -1,3 +1,3 @@
-./stress_social.exe sim_stress_social_1_20241022_143108 200 20 30000 0.1 0.1 1 1 0.1 0.1 0.3 0.4 0.1 0.1
-./stress_social.exe sim_stress_social_2_20241022_143108 200 20 30000 0.1 0.1 5 1 0.1 0.1 0.3 0.4 0.1 0.1
-./stress_social.exe sim_stress_social_3_20241022_143108 200 20 30000 0.1 0.1 10 1 0.1 0.1 0.3 0.4 0.1 0.1
+./stress_social.exe sim_stress_social_1_20241022_152840 200 20 30000 0.1 0.1 1 1 0.1 0.1 0.3 0.4 0.1 0.1
+./stress_social.exe sim_stress_social_2_20241022_152840 200 20 30000 0.1 0.1 1 5 0.1 0.1 0.3 0.4 0.1 0.1
+./stress_social.exe sim_stress_social_3_20241022_152840 200 20 30000 0.1 0.1 1 10 0.1 0.1 0.3 0.4 0.1 0.1
