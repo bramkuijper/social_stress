63c7224b27e1d90a2f7d083da302c546641edd87
emilygilford <151047174+emilygilford@users.noreply.github.com>
2026-05-28 15:22:10 +0100

    Add fecundity for vigilance diagnostics

--- src/ibm/main.cpp
+++ src/ibm/main.cpp
@@ -14,10 +14,11 @@ int main(int argc, char **argv)
     pars.mv = std::stod(argv[8]); // weight of vigilance-related mortality
     pars.p_mig = std::stod(argv[9]); // migration probability
     pars.p_attack = std::stod(argv[10]); // probability of being attacked when predator present
-    pars.init_v = std::stod(argv[11]); // initial vigilance
-    pars.init_stress_hormone_level = std::stod(argv[12]); // initial stress hormone level
-    pars.g = std::stod(argv[13]); // damage removal per timestep
-    pars.k = std::stod(argv[14]); // increase in damage due to hormone != optimum
+    pars.fecundity_power = std::stod(argv[11]); // power of fecundity cost of vigilance
+    pars.init_v = std::stod(argv[12]); // initial vigilance
+    pars.init_stress_hormone_level = std::stod(argv[13]); // initial stress hormone level
+    pars.g = std::stod(argv[14]); // damage removal per timestep
+    pars.k = std::stod(argv[15]); // increase in damage due to hormone != optimum
     
     StressSocial sim_object(pars);
 
